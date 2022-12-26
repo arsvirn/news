60d4bbbd7b679c31ab513e6a0f122b1808bfac77
arsvirn <arsvirn@gmail.com>
2022-12-26 16:10:31 +0300

    action-2

---
+++ app/src/main/java/com/example/news/ui/details/DetailFragment.kt
@@ -0,0 +1,22 @@
+package com.example.news.ui.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.news.R
+import com.example.news.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
