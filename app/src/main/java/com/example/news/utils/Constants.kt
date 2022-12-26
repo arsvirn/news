8bdd290171ed9ce16d37317c153f1204359c7fae
arsvirn <arsvirn@gmail.com>
2022-12-26 17:21:00 +0300

    action-3

---
+++ app/src/main/java/com/example/news/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.news.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "875ca5fc38154cea872c6340a9c783cc"
+    }
+}
