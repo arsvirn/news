345dda8d04e0adcc02a488adff57b194b947e456
unknown <arsvirn@gmail.com>
2023-01-04 08:48:14 +0300

    first commit

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.news.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,19 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_splash)
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(5000)
-            _binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(mBinding.root)
-            bottom_nav_menu.setupWithNavController(
-                navController = nav_host_fragment.findNavController()
-            )
+        setContentView(R.layout.fragment_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
 }
--- app/src/main/java/com/example/news/data/api/NewsService.kt
+++ app/src/main/java/com/example/news/data/api/NewsService.kt
@@ -8,13 +8,6 @@ import retrofit2.http.Query
 
 interface NewsService {
 
-    @GET("/v2/everything")
-    suspend fun getEverything(
-        @Query("q") query: String,
-        @Query("page") page: Int = 1,
-        @Query("apiKey") apiKey: String = API_KEY
-    ) : Response<NewsResponse>
-
     @GET("/v2/top-headlines")
     suspend fun getHeadlines(
         @Query("country") countryCode: String = "ru",
